5ed37344f21fd515543aeae4fcab783e3ddafecb
smeaudre.ext <smeaudre.ext@free-mobile.fr>
2025-01-13 15:30:07 +0100

    Change icon attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
         { name: "viewport", content: "width=device-width, initial-scale=1" },
       ],
       link: [
-        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
         {
           rel: "apple-touch-icon",
           type: "image/png",
